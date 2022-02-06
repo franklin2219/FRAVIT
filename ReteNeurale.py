9e156881372778b1dd4f6b13624971844115e40b
franklin2219 <f.sasso16@studenti.uniba.it>
2022-02-06 12:19:47 +0100

    inseriti dataset e spiegazione progetto

---
+++ ReteNeurale.py
@@ -0,0 +1,54 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors._classification import KNeighborsClassifier
+from sklearn.metrics import mean_absolute_error;
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from numpy import mean
+from numpy import std
+from sklearn import metrics
+from sklearn.neural_network._multilayer_perceptron import MLPClassifier
+
+if __name__ == '__main__':
+    film = pd.read_csv(r"/Users/srancescosasso/SimulatoreNoleggioAcquisto/dataset.csv")
+    print(film)
+'''''
+    #divido il dataset in dati da usare per addestarre(x), e dati da predire(y)
+    y = film.Prezzo
+    x = film["voto_medio"]+film["anno"]+film["durata"]
+    #definisco che sia x che y non sono interi
+    y=y.astype('float')
+    x=x.astype('float')
+
+    #prepare the cross-validation procedure
+    cv = KFold(n_splits=10, random_state=1, shuffle=True)
+    # create model
+    model = MLPClassifier(10)
+    # evaluate model
+    scores = cross_val_score(model, x, y, scoring='accuracy', cv=cv, n_jobs=-1)
+    # report performance
+    print('Accuratezza: %f con deviazione di +/- (%.3f)' % (mean(scores), std(scores)))
+
+    #divido x e y in modo da avere una parte per il training e una per il test
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+
+    #model = KNeighborsClassifier(10)
+    #qui il modello apprende da solo
+    model.fit(x_train, y_train)
+    #effettuo predizione sui dati
+    p_train = model.predict(x_train)#p_train contiene le predizioni(y) sui dati x di training
+    p_test = model.predict(x_test)#p_test contiene le predizioni(y) sui dati x di test
+
+    #calcolo accuratezza del sistema
+    print(f'L\'accuratezza reale sulle predizioni fatte e\' di {metrics.balanced_accuracy_score(y_test, p_test)}')
+
+    #calcolo errori
+    mae_train = mean_absolute_error(y_train, p_train)
+    mae_test = mean_absolute_error(y_test, p_test)
+
+    print('MAE test', mae_test)
+    print('MAE train', mae_train)
+'''
+pass
+
+
---
+++ User.py
@@ -0,0 +1,54 @@
+#Iterazione con l'utente per l'inserimento dei film nella piattaforma
+class Utente:
+    def __init__(self):
+        def comunicazione_iniziale():
+            comunicazione_iniziale=input(f"Vuoi inserire un nuovo film/serieTV? :")
+
+        def titolo_Originale():
+            titolo_Originale=input(f"Inserisci il titolo originale del film/serieTV :")
+
+        def titolo_italiano():
+            titolo_italiano=input(f"Inserisci il titolo tradotto in italiano :")
+
+        def anno_pubblicazione():
+            anno=input(f"Inserisci l'anno di pubblicazione del film/serieTV :")
+
+        def genere():
+            genere=input(f"Inserisci il genere del film/serieTV :")
+
+        def durata():
+            durata=input(f"Definisci la durata del film/serieTV :")
+
+        def attori():
+            attori=input(f"Inserisci gli attori presenti :")
+
+        def registi():
+            registi=input(f"Inserisci i registi :")
+
+        def voto_critica():
+            voto_critica=input("Inserisci il voto della critica (numerico) : ")
+
+        def voto_pubblico():
+            votoPubblico=input("Inserisci il voto del pubblico (numerico) : ")
+
+        def descrizione():
+            descrizione=input(f"Inserisci la descrizione : ")
+
+        def note():
+            note=input(f"Inserisci delle note del film/serieTV : ")
+
+        def humor():
+            humero=input("Inserisci il valore il livello di Humor tra 0 a 5 : ")
+
+        def ritmo():
+            ritmo=input("Inserisci il valore il livello di ritmo tra 0 a 5 : ")
+
+        def impegno():
+            impegno=input("Inserisci il valore il livello di impegno tra 0 a 5 : ")
+
+        def tensione():
+            tensione=input("Inserisci il valore il livello di tensione tra 0 a 5 : ")
+
+        def erotismo():
+            erotismo=input("Inserisci il valore il livello di erotismo tra 0 a 5 : ")
+
