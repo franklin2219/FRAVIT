bc7caab3a5b5e351d36f84f2a42ca8ea6bda91be
Vito Musco <v.musco1@studenti.uniba.it>
2022-02-07 11:47:20 +0100

    Aggiornato utente

--- SchermataIniziale.py
+++ SchermataIniziale.py
@@ -2,8 +2,8 @@ from User import Utente
 from ReteNeurale import PrevisionePrezzo
 
 if __name__ == '__main__':
-    utente1 = Utente
-    prezzo = PrevisionePrezzo
+    utente1 = Utente()
+    prezzo = PrevisionePrezzo()
     scelta=0
 
     print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
@@ -18,18 +18,7 @@ if __name__ == '__main__':
         scelta=input("Inserisci il numero dell'operazione da effettuare : ")
 
     if scelta == '1':
-        utente1.titolo_Originale("")
-        utente1.titolo_italiano("")
-        utente1.anno_pubblicazione("")
-        utente1.genere("")
-        utente1.durata("")
-        utente1.voto_critica("")
-        utente1.voto_pubblico("")
-        utente1.ritmo("")
-        utente1.erotismo("")
-        utente1.tensione("")
-        utente1.impegno("")
-        utente1.humor("")
+        utente1.inserisciFilm()
 
 
 
--- User.py
+++ User.py
@@ -1,51 +1,54 @@
 #Iterazione con l'utente per l'inserimento dei film nella piattaforma
 class Utente:
     def __init__(self):
-        Cioa="aws"
-    def titolo_Originale(self):
+        titolo = ""
+        titolo_italiano = ""
+        anno = 0
+        genere = ""
+        durata = 0
+        voto = 0
+        humor = 0
+        ritmo = 0
+        impegno = 0
+        tensione = 0
+        erotismo = 0
+        peso_anno = 0
+        peso_voto = 0
+        peso_durata = 0
+    @classmethod
+    def inserisciFilm(self):
         titolo=input(f"Inserisci il titolo originale del film/serieTV :")
-        return titolo
-
-    def titolo_italiano(self):
         titolo_italiano=input(f"Inserisci il titolo tradotto in italiano :")
-        return titolo_italiano
-
-    def anno_pubblicazione(self):
         anno=input(f"Inserisci l'anno di pubblicazione del film/serieTV :")
-        return anno
-
-    def genere(self):
         genere=input(f"Inserisci il genere del film/serieTV :")
-        return genere
-
-    def durata(self):
         durata=input(f"Definisci la durata del film/serieTV :")
-        return durata
-
-    def voto_critica(self):
-        voto_critica=input("Inserisci il voto della critica (numerico) : ")
-        return voto_critica
-
-    def voto_pubblico(self):
-        votoPubblico=input("Inserisci il voto del pubblico (numerico) : ")
-        return votoPubblico
-
-    def humor(self):
+        voto=input("Inserisci il voto (numerico) :")
         humor=input("Inserisci il valore il livello di Humor tra 0 a 5 : ")
-        return humor
-
-    def ritmo(self):
         ritmo=input("Inserisci il valore il livello di ritmo tra 0 a 5 : ")
-        return ritmo
-
-    def impegno(self):
         impegno=input("Inserisci il valore il livello di impegno tra 0 a 5 : ")
-        return impegno
-
-    def tensione(self):
         tensione=input("Inserisci il valore il livello di tensione tra 0 a 5 : ")
-        return tensione
-
-    def erotismo(self):
         erotismo=input("Inserisci il valore il livello di erotismo tra 0 a 5 : ")
-        return erotismo
+
+        if anno >= "2021":
+            peso_anno = 4
+        elif anno >= 2012:
+            peso_anno = 3
+        elif anno >= 2002:
+            peso_anno = 2
+        else: peso_anno = 1
+
+        if durata < "90":
+            peso_durata = 4
+        elif durata < "120":
+            peso_durata = 3
+        elif durata < "140":
+            peso_durata = 2
+        else: peso_durata = 1
+
+        if voto >= "8":
+            peso_voto = 4
+        elif voto >= 5:
+            peso_voto = 3
+        elif voto >= 3:
+            peso_voto = 2
+        else: peso_voto = 1
