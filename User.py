426426525d608740b2a08335cfffe9c0d3b4b8a6
franklin2219 <f.sasso16@studenti.uniba.it>
2022-02-07 16:13:20 +0100

    gianni ha pushato

--- User.py
+++ User.py
@@ -19,15 +19,23 @@ class Utente:
     def inserisciFilm(self):
         self.titolo=input(f"Inserisci il titolo originale del film/serieTV :")
         self.titolo_italiano=input(f"Inserisci il titolo tradotto in italiano :")
-        self.anno=int(input("Inserisci l'anno di pubblicazione del film/serieTV :"))
+        while self.anno <= 1885 or self.anno >= 2023:
+            self.anno=int(input("Inserisci l'anno di pubblicazione del film/serieTV :"))
         self.genere=input(f"Inserisci il genere del film/serieTV :")
-        self.durata=int(input("Definisci la durata del film/serieTV :"))
-        self.voto=int(input("Inserisci il voto (numerico) :"))
-        self.humor=input("Inserisci il valore il livello di Humor tra 0 a 5 : ")
-        self.ritmo=input("Inserisci il valore il livello di ritmo tra 0 a 5 : ")
-        self.impegno=input("Inserisci il valore il livello di impegno tra 0 a 5 : ")
-        self.tensione=input("Inserisci il valore il livello di tensione tra 0 a 5 : ")
-        self.erotismo=input("Inserisci il valore il livello di erotismo tra 0 a 5 : ")
+        while self.durata >= 180 or self.durata <= 30:
+            self.durata=int(input("Definisci la durata del film/serieTV :"))
+        while self.voto <=1 or self.voto >= 10:
+            self.voto=int(input("Inserisci il voto (un numero da 1 a 10) :"))
+        while self.humor <=0 or self.humor >= 5:
+            self.humor=int(input("Inserisci il valore il livello di Humor tra 0 a 5 : "))
+        while self.ritmo <=0 or self.ritmo >= 5:
+            self.ritmo=int(input("Inserisci il valore il livello di ritmo tra 0 a 5 : "))
+        while self.impegno <=0 or self.impegno >= 5:
+            self.impegno=int(input("Inserisci il valore il livello di impegno tra 0 a 5 : "))
+        while self.tensione <=0 or self.tensione >= 5:
+            self.tensione=int(input("Inserisci il valore il livello di tensione tra 0 a 5 : "))
+        while self.erotismo <=0 or self.voto >= 5:
+            self.erotismo=int(input("Inserisci il valore il livello di erotismo tra 0 a 5 : "))
 
         if self.anno >= 2021:
             self.peso_anno = 4
