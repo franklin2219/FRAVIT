829311250b113e0caea9ce994d024db2b62db4ca
Vito Musco <v.musco1@studenti.uniba.it>
2022-02-07 19:36:03 +0100

    Aggiunto knn

--- User.py
+++ User.py
@@ -66,7 +66,7 @@ class Utente:
             cls.peso_voto = 1
 
     @classmethod
-    def richiestafilm(cls, self):
+    def richiestafilm(cls):
         print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
         print("+                  1)Animazione                                    +")
         print("+                  2)Drammatico                                    +")
@@ -96,86 +96,27 @@ class Utente:
         print("+                  26)Sperimentale                                 +")
         print("+                  27)Cortometraggio                               +")
         print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        while self.genere_numerico <= 0 or self.genere_numerico >= 28:
-            self.genere_numerico = int(input("Inserisci il genere di Film/SerieTv che ti piacerebbe vedere :"))
+        while cls.genere_numerico <= 0 or cls.genere_numerico >= 28:
+            cls.genere_numerico = int(input("Inserisci il genere di Film/SerieTv che ti piacerebbe vedere :"))
 
-        while self.humor < 0 or self.humor > 5:
-            self.humor = int(input("Inserisci il valore il livello di Humor tra 0 a 5 :"))
+        while cls.humor < 0 or cls.humor > 5:
+            cls.humor = int(input("Inserisci il valore il livello di Humor tra 0 a 5 :"))
 
-        while self.ritmo < 0 or self.ritmo > 5:
-            self.ritmo = int(input("Inserisci il valore il livello di ritmo tra 0 a 5 :"))
+        while cls.ritmo < 0 or cls.ritmo > 5:
+            cls.ritmo = int(input("Inserisci il valore il livello di ritmo tra 0 a 5 :"))
 
-        while self.impegno < 0 or self.impegno > 5:
-            self.impegno = int(input("Inserisci il valore il livello di impegno tra 0 a 5 :"))
+        while cls.impegno < 0 or cls.impegno > 5:
+            cls.impegno = int(input("Inserisci il valore il livello di impegno tra 0 a 5 :"))
 
-        while self.tensione < 0 or self.tensione > 5:
-            self.tensione = int(input("Inserisci il valore il livello di tensione tra 0 a 5 :"))
+        while cls.tensione < 0 or cls.tensione > 5:
+            cls.tensione = int(input("Inserisci il valore il livello di tensione tra 0 a 5 :"))
 
-        while self.erotismo < 0 or self.erotismo > 5:
-            self.erotismo = int(input("Inserisci il valore il livello di erotismo tra 0 a 5 :"))
+        while cls.erotismo < 0 or cls.erotismo > 5:
+            cls.erotismo = int(input("Inserisci il valore il livello di erotismo tra 0 a 5 :"))
 
-        while self.peso_anno < 1 or self.peso_anno > 4:
+        while cls.peso_anno < 1 or cls.peso_anno > 4:
             print("Inserisci il periodo cinematografico che preferisci:")
             print("1: piu' di 20 anni fa")
             print("2: dal 2002 al 2012")
             print("3: dal 2012 al 2021")
-            print("4: dal 2021 in poi")
-            self.peso_anno = int(input())
-
-
-'''
-        if self.genere=="Animazione":
-            genere_numerico = 1
-        elif self.genere=="Drammatico":
-            genere_numerico = 2
-        elif self.genere=="Sentimentale":
-            genere_numerico = 3
-        elif self.genere=="Commedia":
-            genere_numerico = 4
-        elif self.genere=="Poliziesco":
-            genere_numerico = 5
-        elif self.genere=="Thriller":
-            genere_numerico = 6
-        elif self.genere=="Avventura":
-            genere_numerico = 7
-        elif self.genere=="Documentario":
-            genere_numerico = 8
-        elif self.genere=="Horror":
-            genere_numerico = 9
-        elif self.genere=="Azione":
-            genere_numerico = 10
-        elif self.genere=="Catastrofico":
-            genere_numerico = 11
-        elif self.genere=="Western":
-            genere_numerico = 12
-        elif self.genere=="Spionaggio":
-            genere_numerico = 13
-        elif self.genere=="Biografico":
-            genere_numerico = 14
-        elif self.genere=="Musicale":
-            genere_numerico = 15
-        elif self.genere=="Fantasy":
-            genere_numerico = 16
-        elif self.genere=="Guerra":
-            genere_numerico = 17
-        elif self.genere=="Grottesco":
-            genere_numerico = 18
-        elif self.genere=="Gangster":
-            genere_numerico = 19
-        elif self.genere=="Mitologico":
-            genere_numerico = 20
-        elif self.genere=="Storico":
-            genere_numerico = 21
-        elif self.genere=="Noir":
-            genere_numerico = 22
-        elif self.genere=="Supereroi":
-            genere_numerico = 23
-        elif self.genere=="Biblico":
-            genere_numerico = 24
-        elif self.genere=="Sportivo":
-            genere_numerico = 25
-        elif self.genere=="Sperimentale":
-            genere_numerico = 26
-        elif self.genere=="Cortometraggio":
-            genere_numerico = 27
-'''
+            cls.peso_anno = int(input("4: dal 2021 in poi: "))
